e1ffcfde34c7119a61727c556290c313268b705c
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-11-26 15:00:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown('# Welcome to SQL on the fly')
+st.markdown('## An app to analyze your csv, as you go')
+
+with st.sidbar:
+  st.markdown(' ### Upload your csv')
