48166bc9ee2b8a475a22e9d931bd04ec912464b0
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-11-26 15:07:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ import streamlit as st
 st.markdown('# Welcome to SQL on the fly')
 st.markdown('## An app to analyze your csv, as you go')
 
-with st.sidbar:
+with st.sidbear:
   st.markdown(' ### Upload your csv')
+  file = st.file_uploader('Add file here')
+  add = st.button('Add csv')
+
+
+if add:
+  df = pd.read_csv(file)
+  print(df.head())
