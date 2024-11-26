396b8d9c6e87002f3b0cf82b4e64f3088403d1bf
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-11-26 15:24:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pandasql as ps
 
 st.markdown('# Welcome to SQL on the fly')
 st.markdown('## An app to analyze your csv, as you go')
@@ -10,9 +11,15 @@ with st.sidebar:
   if not delimiter:
     delimiter = ';'
   file = st.file_uploader('Add file here')
+  tablename = st.text_input('name of table')
   add = st.button('Add csv')
 
 
 if add:
   df = pd.read_csv(file, delimiter=delimiter)
-  st.dataframe(df)
+  query = st.text_area('Query')
+  mapping = {tablename, df}
+  result = ps.sqldf(query, mapping)
+  if query:
+    st.dataframe(result)
+
